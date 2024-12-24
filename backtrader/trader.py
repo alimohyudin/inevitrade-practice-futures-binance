c613a852884a8eebbcf1bb4106a3361d4852a1b7
Ali Mohyudin <mohyudin14@gmail.com>
2024-12-24 18:05:08 +0500

    working with 3min data

--- backtrader/trader.py
+++ backtrader/trader.py
@@ -1,4 +1,7 @@
 import backtrader as bt
+import datetime
+
+total_closed_positions = 0
 
 class MACDStrategy(bt.Strategy):
     params = (
@@ -18,10 +21,13 @@ class MACDStrategy(bt.Strategy):
         ('start_date', None),
         ('end_date', None),
     )
+    
 
-    def log(self, txt, dt=None):
-        dt = dt or self.data.datetime.datetime(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+    def log(self, txt, num):
+        dt = self.datas[0].datetime.datetime(0)
+        #print(self.datas[0].datetime.datetime(0))
+        
+        print(f'{dt}, {txt}, {num}, {self.position.size}')
     def __init__(self):
         # Indicators
         self.rsi = bt.indicators.RSI_Safe(self.data.close, period=self.params.rsi_period)
@@ -43,7 +49,7 @@ class MACDStrategy(bt.Strategy):
         # if self.params.start_date and self.params.end_date:
         #     if not (self.params.start_date <= self.datetime.date() <= self.params.end_date):
         #         return
-        self.log(f'Close: {self.data.close[0]}')
+        # self.log(f'Close: {self.data.close[0]}')
         
         # Update `barssince` counters
         if self.rsi[0] <= self.params.rsi_oversold:
@@ -66,31 +72,36 @@ class MACDStrategy(bt.Strategy):
         self.sell_signal = was_overbought and crossover_bear
 
         
-
-        # self.buy()
-        # self.sell()
         # Long Strategy
         if self.buy_signal and self.params.enable_long_strategy:
-            print("was_oversold=", self.rsi[0], self.params.rsi_oversold, was_oversold)
             self.close_short()
             if not self.position:
-                print("buy")
-                self.buy()
+                print("==================================")
+                self.log("LONG", self.data.close[0])
+                self.buy(size=cerebro.broker.getcash() / self.data.close[0], exectype=bt.Order.Market)
+                
 
         # Short Strategy
         if self.sell_signal and self.params.enable_short_strategy:
-            print("was_overbought=", self.rsi[0], self.params.rsi_overbought, was_overbought)
             self.close_long()
             if not self.position:
-                print("sell")
-                self.sell()
+                print("==================================")
+                self.log("SHORT", self.data.close[0])
+                self.sell(size=cerebro.broker.getcash() / self.data.close[0], exectype=bt.Order.Market)
+                
 
     def close_long(self):
         if self.position.size > 0:
+            self.log("closing LONG", self.data.close[0])
+            global total_closed_positions
+            total_closed_positions += 1
             self.close()
 
     def close_short(self):
         if self.position.size < 0:
+            self.log("closing SHORT", self.data.close[0])
+            global total_closed_positions
+            total_closed_positions += 1
             self.close()
 
 # Data preparation
@@ -99,6 +110,7 @@ data = bt.feeds.GenericCSVData(
     dataname='./backtrader/BTCUSDT.csv',  # Replace with your data file path
     dtformat='%m-%d-%YT%H:%M:%S.000Z',  # New format to match '2024-12-01T00:00:00.000Z'
     timeframe=bt.TimeFrame.Minutes,
+    fromdate=datetime.datetime(2024, 12, 22),
     compression=1,
     openinterest=-1,
 )
@@ -108,11 +120,12 @@ cerebro.adddata(data)
 cerebro.addstrategy(MACDStrategy)
 
 # Run
-cerebro.broker.setcash(200.0)
+cerebro.broker.setcash(1000.0)
 cerebro.broker.setcommission(commission=0.0)
 print(f'Starting Portfolio Value: {cerebro.broker.getvalue()}')
 cerebro.run()
 print(f'Ending Portfolio Value: {cerebro.broker.getvalue()}')
+print(f'Total Closed Positions: {total_closed_positions}')
 
 # Visualization
 cerebro.plot()
